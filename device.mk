3a61e80e7213350e915df510464d64580e1513f1
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-06-01 00:35:02 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # API
-PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_TARGET_VNDK_VERSION := 31
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # A/B
